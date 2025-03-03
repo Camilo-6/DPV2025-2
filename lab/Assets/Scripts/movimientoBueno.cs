5f6c6eeeef7e771d6629fdc3cd9cc27ff12deb32
Camilo <jcamilo@ciencias.unam.mx>
2025-03-03 17:48:08 -0600

    lab 03/03/25

---
+++ lab/Assets/Scripts/movimientoBueno.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using System.Collections;
+
+public class movimientoBueno : MonoBehaviour
+{
+    // Character controller
+    [SerializeField] private CharacterController cc;
+    // Direccion
+    [SerializeField] private Vector3 direccion;
+    // Gravedad
+    [SerializeField] private float gravedad = 10.0f;
+    // Rotacion
+    [SerializeField] private float rotacion;
+    // Velocidad de rotacion
+    [SerializeField] private float velocidadRotacion = 5.0f;
+    // Salto
+    [SerializeField] private float salto = 400.0f;
+    // Velocidad
+    [SerializeField] private float velocidad = 5.0f;
+    // Animator
+    [SerializeField] private Animator anim;
+    // Vertical
+    [SerializeField] private float vertical;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        this.cc = this.gameObject.GetComponent<CharacterController>();
+        this.anim = this.gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cc.isGrounded)
+        {
+            anim.SetTrigger("suelo");
+            vertical = Input.GetAxisRaw("Vertical");
+            direccion = gameObject.transform.TransformDirection(new Vector3(0, 0, vertical) * velocidad);
+            rotacion = Input.GetAxis("Horizontal") * velocidadRotacion;
+            anim.SetFloat("move", vertical);
+            if (vertical == 0)
+            {
+                anim.SetBool("idle?", true);
+            }
+            else
+            {
+                anim.SetBool("idle?", false);
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                direccion.y += salto * Time.deltaTime;
+                cc.transform.Rotate(Vector3.zero); // evita que el personaje rote en el aire
+                anim.SetTrigger("salto");
+            }
+        }
+        else
+        {
+            anim.ResetTrigger("suelo");
+        }
+        direccion -= new Vector3(0, gravedad * Time.deltaTime, 0);
+        cc.transform.Rotate(new Vector3(0, rotacion, 0));
+        cc.Move(direccion * Time.deltaTime);
+    }
+}
