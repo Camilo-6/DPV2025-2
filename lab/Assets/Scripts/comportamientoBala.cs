9e5a38ba12f9ddd4818c7ab950287debec0eceb1
Camilo <jcamilo@ciencias.unam.mx>
2025-03-11 08:43:26 -0600

    modificacion a las balas

--- lab/Assets/Scripts/comportamientoBala.cs
+++ lab/Assets/Scripts/comportamientoBala.cs
@@ -17,4 +17,18 @@ public class comportamientoBala : MonoBehaviour
     }
 
     // Quitamos el update ya que no lo necesitamos
+
+    // Cuando colisiona con otro objeto
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("enemigo destruible") || other.gameObject.CompareTag("objeto destruible"))
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+        else if (other.gameObject.CompareTag("escenario"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
 }
